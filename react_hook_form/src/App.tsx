8c9469ea63ab4d8cf41ddeeb2ece4735d169a091
thalyson <thalysonlima820@gmail.com>
2024-08-08 15:31:35 -0300

    aula final

---
+++ react_hook_form/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Form from './components/Form'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='font-bold text-3xl'>React Hook Form com TS </h1>
+     <Form />
+    </>
+  )
+}
+
+export default App
